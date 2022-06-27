80c4cb19e8bb0819aac09585c8a0ae77feb9f598
yozooCN <nhzouyonghang@gmail.com>
2022-06-27 14:53:26 +0800

    change src

---
+++ assets/Rc.eee3155a.js
@@ -0,0 +1,1 @@
+import{u as t,j as e,R as o}from"./index.842eb5f0.js";const s=()=>(t(),e(o,{}));export{s as default};
--- assets/Rc.f36060aa.js
+++
@@ -1,1 +0,0 @@
-import{u as t,j as e,R as o}from"./index.d18e0018.js";const s=()=>(t(),e(o,{}));export{s as default};
