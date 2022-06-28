304ab28b95827b328b7301eecef05a58ae396c59
yozooCN <nhzouyonghang@gmail.com>
2022-06-28 14:36:05 +0800

    delete

--- assets/Rc.79a43ef7.js
+++
@@ -1,1 +0,0 @@
-import{u as t,j as e,R as o}from"./index.6a113bf4.js";const s=()=>(t(),e(o,{}));export{s as default};
--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/kongtiao/sw.js', { scope: '/kongtiao/' })})}
